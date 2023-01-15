7240a10d603633af5acc0d9c339171437c48af08
SOURAVSHEEL <122149563+SOURAVSHEEL@users.noreply.github.com>
2023-01-15 14:32:56 +0530

    ASSIGNEMENT OF REVERSEING THE WORD

---
+++ _2_assignment.py
@@ -0,0 +1,8 @@
+# Write a Python program that accepts a word from the user and reverse it.
+
+users_input = str(input("Write the any word: "))
+print(users_input)
+
+reversed_order = users_input[::-1]
+
+print(reversed_order)
